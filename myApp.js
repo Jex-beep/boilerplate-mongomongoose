8a18899723fbbef74e7b19993ac07ebf035df46c
Jex-beep <jmppunsalan@gmail.com>
2025-10-03 01:05:47 +0800

    lol

--- myApp.js
+++ myApp.js
@@ -1,59 +1,128 @@
-require('dotenv').config();
+require("dotenv").config();
 
+let mongoose = require("mongoose");
 
-let Person;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    // Keeping these options to silence old Mongoose warnings, 
+    // even though the underlying driver may ignore them.
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Successfully connected to MongoDB"))
+  .catch((err) => console.error("Connection error", err));
 
+let personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+let Person = mongoose.model("Person", personSchema);
+
+// FIX 1: Must call done(err) on error, not return err
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const person = new Person({
+    name: "John Doe",
+    age: 25,
+    favoriteFoods: ["pizza", "burgers"],
+  });
+
+  person.save(function (err, data) {
+    if (err) return done(err); // Changed: Must call done(err)
+    done(null, data);
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return done(err);
+    done(null, people);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, function (err, peopleFound) {
+    if (err) return done(err);
+    done(null, peopleFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, function (err, personFound) {
+    if (err) return done(err);
+    done(null, personFound);
+  });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function (err, personFound) {
+    if (err) return done(err);
+    done(null, personFound);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if (err) return done(err);
+    if (!person) return done(new Error("No person found")); // Good error handling
+
+    person.favoriteFoods.push(foodToAdd);
+
+    person.save((err, updatedPerson) => {
+      if (err) return done(err);
+      done(null, updatedPerson);
+    });
+  });
 };
 
+// FIX 2: Removed $set operator for simplification (not required by the test)
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet }, // Simplified update object
+    { new: true },
+    (err, updatedPerson) => {
+      if (err) return done(err);
+      done(null, updatedPerson);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  // Mongoose method is findByIdAndRemove, not findByIdAndRemove (case sensitive)
+  Person.findByIdAndRemove(personId, (err, removedPerson) => {
+    if (err) return done(err);
+    done(null, removedPerson);
+  });
 };
 
+// FIX 3: Changed deprecated Person.remove to modern Person.deleteMany
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.deleteMany({ name: nameToRemove }, (err, result) => {
+    if (err) return done(err);
+    done(null, result);
+  });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  
+  Person.find({ favoriteFoods: foodToSearch }) // Finds people who like 'burrito'
+    .sort({ name: 1 })                       // Sorts them by name ascending
+    .limit(2)                                // LIMIT set to TWO documents, as requested
+    .select("-age")                           // Hides the 'age' field
+    .exec((err, data) => {                   // Executes the query
+      if (err) return done(err);
+      done(null, data);
+    });
 };
-
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
